87a025715b7d9f0bdfa1ff0f7ff0193615f24a76
jeedeekay <gavinkinder@gmail.com>
2022-12-15 19:24:00 -0800

    First Commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import 'font-awesome/css/font-awesome.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
